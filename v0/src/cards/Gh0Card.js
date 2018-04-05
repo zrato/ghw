cb24fafc1617d08dad4c62f536d5d6669a352e9e
Michael Angerman <stormasm@gmail.com>
2018-04-05 13:44:58 -0700

    Gh0 card is up and running

---
+++ v0/src/cards/Gh0Card.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from 'material-ui/styles';
+import Card, { CardContent, CardMedia } from 'material-ui/Card';
+import Typography from 'material-ui/Typography';
+
+const styles = {
+  card: {
+    maxWidth: 175,
+  },
+  media: {
+    height: 90,
+  },
+};
+
+function Gh0Card(props) {
+  const { classes } = props;
+  return (
+    <div>
+      <Card className={classes.card}>
+        <CardMedia
+          className={classes.media}
+          image="https://avatars3.githubusercontent.com/u/1809991?s=400&v=4"
+          title="stormasm"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="headline" component="p">
+            <a href="https://github.com/stormasm">stormasm</a>
+          </Typography>
+          <Typography component="p">
+            Michael Angerman <b>Corvallis, Oregon</b>
+          </Typography>
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
+
+Gh0Card.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(Gh0Card);
--- v0/src/index.js
+++ v0/src/index.js
@@ -6,6 +6,8 @@ import { render } from 'react-dom';
 //import Demo from './cards/SimpleMediaCard';
 //import Demo from './cards/MediaControlCard';
 //import Demo from './cards/RecipeReviewCard';
+//
+import Demo from './cards/Gh0Card';
 // grid-list
 //import Demo from './grid-list/AdvancedGridList';
 //import Demo from './grid-list/ImageGridList';
@@ -14,7 +16,7 @@ import { render } from 'react-dom';
 //
 //import Demo from './grid-list/SimpleCardGL';
 //import Demo from './grid-list/SimpleMediaCardGL';
-import Demo from './grid-list/RecipeReviewCardGL';
+//import Demo from './grid-list/RecipeReviewCardGL';
 
 const rootElement = document.querySelector('#root');
 if (rootElement) {
