7e97eb1910e7ab8bb445327ab37cbcdcdf018312
Michael Angerman <stormasm@gmail.com>
2018-04-05 09:33:23 -0700

    initial commit

---
+++ v0/src/grid-list/tileData.js
@@ -0,0 +1,69 @@
+const tileData = [
+  {
+    img: 'https://avatars3.githubusercontent.com/u/1809991?s=400&v=4',
+    title: 'Breakfast',
+    author: 'jill111',
+    cols: 2,
+    featured: true,
+  },
+  {
+    img: 'https://avatars3.githubusercontent.com/u/1809991?s=400&v=4',
+    title: 'Tasty burger',
+    author: 'director90',
+  },
+  {
+    img: 'https://avatars3.githubusercontent.com/u/1809991?s=400&v=4',
+    title: 'Camera',
+    author: 'Danson67',
+  },
+  {
+    img: 'https://avatars3.githubusercontent.com/u/1809991?s=400&v=4',
+    title: 'Morning',
+    author: 'fancycrave1',
+    featured: true,
+  },
+  {
+    img: 'https://avatars3.githubusercontent.com/u/1809991?s=400&v=4',
+    title: 'Hats',
+    author: 'Hans',
+  },
+  {
+    img: 'https://avatars3.githubusercontent.com/u/1809991?s=400&v=4',
+    title: 'Honey',
+    author: 'fancycravel',
+  },
+  {
+    img: 'https://avatars3.githubusercontent.com/u/1809991?s=400&v=4',
+    title: 'Vegetables',
+    author: 'jill111',
+    cols: 2,
+  },
+  {
+    img: 'https://avatars3.githubusercontent.com/u/1809991?s=400&v=4',
+    title: 'Water plant',
+    author: 'BkrmadtyaKarki',
+  },
+  {
+    img: 'https://avatars3.githubusercontent.com/u/1809991?s=400&v=4',
+    title: 'Mushrooms',
+    author: 'PublicDomainPictures',
+  },
+  {
+    img: 'https://avatars3.githubusercontent.com/u/1809991?s=400&v=4',
+    title: 'Olive oil',
+    author: 'congerdesign',
+  },
+  {
+    img: 'https://avatars3.githubusercontent.com/u/1809991?s=400&v=4',
+    title: 'Sea star',
+    cols: 2,
+    author: '821292',
+  },
+  {
+    img: 'https://avatars3.githubusercontent.com/u/1809991?s=400&v=4',
+    title: 'Bike',
+    author: 'danfador',
+  },
+];
+
+export default tileData;
---
+++ v0/src/index.js
@@ -0,0 +1,19 @@
+
+import React from 'react';
+import { render } from 'react-dom';
+// cards
+//import Demo from './cards/SimpleCard';
+//import Demo from './cards/SimpleMediaCard';
+//import Demo from './cards/MediaControlCard';
+//import Demo from './cards/RecipeReviewCard';
+// grid-list
+//import Demo from './grid-list/AdvancedGridList';
+//import Demo from './grid-list/ImageGridList';
+//import Demo from './grid-list/SingleLineGridList';
+//import Demo from './grid-list/TitlebarGridList';
+import Demo from './grid-list/CardGridList';
+
+const rootElement = document.querySelector('#root');
+if (rootElement) {
+  render(<Demo />, rootElement);
+}
