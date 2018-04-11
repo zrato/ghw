6b2a3a59ada061a786c0d9bb6651d679d168c428
Michael Angerman <stormasm@gmail.com>
2018-04-11 10:36:27 -0700

    starting to swap out tileData fields for the data from graphql-redis-github

--- v0/src/grid-list/Gh0CardGL.js
+++ v0/src/grid-list/Gh0CardGL.js
@@ -1,28 +1,28 @@
 // This was the ImageGridList prior to swapping out
 // an img for a SimpleCard...
 
-import React from 'react';
-import PropTypes from 'prop-types';
-import { withStyles } from 'material-ui/styles';
-import GridList, { GridListTile } from 'material-ui/GridList';
-import tileData from './tileData';
-import Gh0Card from './../cards/Gh0Card';
+import React from "react";
+import PropTypes from "prop-types";
+import { withStyles } from "material-ui/styles";
+import GridList, { GridListTile } from "material-ui/GridList";
+import tileData from "./tileData1";
+import Gh0Card from "./../cards/Gh0Card";
 
 const styles = theme => ({
   root: {
-    display: 'flex',
-    flexWrap: 'wrap',
-    justifyContent: 'space-around',
-    overflow: 'hidden',
-    backgroundColor: theme.palette.background.paper,
+    display: "flex",
+    flexWrap: "wrap",
+    justifyContent: "space-around",
+    overflow: "hidden",
+    backgroundColor: theme.palette.background.paper
   },
   gridList: {
     width: 600,
-    height: 720,
+    height: 720
   },
   subheader: {
-    width: '100%',
-  },
+    width: "100%"
+  }
 });
 
 function Gh0CardGL(props) {
@@ -32,7 +32,7 @@ function Gh0CardGL(props) {
     <div className={classes.root}>
       <GridList cellHeight={250} className={classes.gridList} cols={3}>
         {tileData.map(tile => (
-          <GridListTile key={tile.img} cols={tile.cols || 1}>
+          <GridListTile key={tile.avatar} cols={tile.cols || 1}>
             <Gh0Card />
           </GridListTile>
         ))}
@@ -42,7 +42,7 @@ function Gh0CardGL(props) {
 }
 
 Gh0CardGL.propTypes = {
-  classes: PropTypes.object.isRequired,
+  classes: PropTypes.object.isRequired
 };
 
 export default withStyles(styles)(Gh0CardGL);
---
+++ v0/src/grid-list/tileData1.js
@@ -0,0 +1,69 @@
+const tileData = [
+  {
+    avatar: 'https://avatars3.githubusercontent.com/u/1809991?s=400&v=4',
+    title: 'Breakfast',
+    author: 'jill111',
+    cols: 1,
+    featured: true,
+  },
+  {
+    avatar: 'https://avatars3.githubusercontent.com/u/1809991?s=400&v=4',
+    title: 'Tasty burger',
+    author: 'director90',
+  },
+  {
+    avatar: 'https://avatars3.githubusercontent.com/u/1809991?s=400&v=4',
+    title: 'Camera',
+    author: 'Danson67',
+  },
+  {
+    avatar: 'https://avatars3.githubusercontent.com/u/1809991?s=400&v=4',
+    title: 'Morning',
+    author: 'fancycrave1',
+    featured: true,
+  },
+  {
+    avatar: 'https://avatars3.githubusercontent.com/u/1809991?s=400&v=4',
+    title: 'Hats',
+    author: 'Hans',
+  },
+  {
+    avatar: 'https://avatars3.githubusercontent.com/u/1809991?s=400&v=4',
+    title: 'Honey',
+    author: 'fancycravel',
+  },
+  {
+    avatar: 'https://avatars3.githubusercontent.com/u/1809991?s=400&v=4',
+    title: 'Vegetables',
+    author: 'jill111',
+    cols: 1,
+  },
+  {
+    avatar: 'https://avatars3.githubusercontent.com/u/1809991?s=400&v=4',
+    title: 'Water plant',
+    author: 'BkrmadtyaKarki',
+  },
+  {
+    avatar: 'https://avatars3.githubusercontent.com/u/1809991?s=400&v=4',
+    title: 'Mushrooms',
+    author: 'PublicDomainPictures',
+  },
+  {
+    avatar: 'https://avatars3.githubusercontent.com/u/1809991?s=400&v=4',
+    title: 'Olive oil',
+    author: 'congerdesign',
+  },
+  {
+    avatar: 'https://avatars3.githubusercontent.com/u/1809991?s=400&v=4',
+    title: 'Sea star',
+    cols: 1,
+    author: '821292',
+  },
+  {
+    avatar: 'https://avatars3.githubusercontent.com/u/1809991?s=400&v=4',
+    title: 'Bike',
+    author: 'danfador',
+  },
+];
+
+export default tileData;
